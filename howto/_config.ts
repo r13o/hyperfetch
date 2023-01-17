614684beb6c1301077e1b140437f0c2ffda0b4e9
Romuald Bulyshko <git42df6@romuald.xyz>
2023-01-17 21:47:44 +0200

    Update styling

--- howto/_config.ts
+++ howto/_config.ts
@@ -1,9 +1,14 @@
 import lume from "lume/mod.ts";
+import inline from "lume/plugins/inline.ts";
+import sass from "lume/plugins/sass.ts";
 
 const site = lume({
     location: new URL("https://howto.hyperfetch.net"),
     prettyUrls: false,
 });
+
+site.use(inline());
+site.use(sass());
 
 site.copy("public", ".");
 
---
+++ howto/index.md
@@ -0,0 +1,5 @@
+---
+title: The simplest way to fetch
+---
+
+Just type &#8220;hyperfetch.net/&#8221; in front of any URL.
